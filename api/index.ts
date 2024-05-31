c2ccf2a51168af7faeda0c7f484d380eb4d7d374
jx06T <50313tjx06@gmail.com>
2024-06-01 01:16:05 +0800

    new file:   .gitignore 	new file:   api/index.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   vercel.json

---
+++ api/index.ts
@@ -0,0 +1,58 @@
+const express = require("express");
+const axios = require('axios');
+// const cors = require("cors")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+function share(key: string, content: string){
+    let data = JSON.stringify({
+        "content": content,
+        "commentPermission": "everyone"
+    });
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.hackmd.io/v1/notes',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': key,
+            'Cookie': 'locale=dev'
+        },
+        data: data
+    };
+    return axios.request(config);
+}
+
+function share0(req, res) {
+    const {authorization} = req.headers;
+    const key = authorization
+    const { content } = req.body;
+
+    // console.log(req.headers)
+    // console.log(key, content)
+
+    share(key, content)
+        .then((response) => {
+            const r = JSON.stringify(response.data)
+            console.log(r);
+            res.status(200).json({ message: "successfully", link: response.data.publishLink });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ error: "Internal server error" });
+        });
+}
+
+//------------------------------------------------------------------------
+
+// app.use(cors({ origin: 'https://jx06blog-jx06ts-projects.vercel.app' }));
+// app.use(cors({origin: 'http://localhost:4321'}));
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send("HI"));
+
+app.post('/share', share0);
+
+module.exports = app;
